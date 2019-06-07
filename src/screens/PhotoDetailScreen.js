6e0a7e0e231423b609c5f49972ac37c2b5a5293f
Felipe Catania <felicatania@gmail.com>
2019-06-07 10:25:16 -0300

    use data from mock server and redo tests

--- src/screens/PhotoDetailScreen.js
+++ src/screens/PhotoDetailScreen.js
@@ -1,13 +1,11 @@
 import React, { PureComponent } from 'react';
-import { View, StyleSheet, Image, StatusBar } from 'react-native';
+import { View, StyleSheet, Image, StatusBar, ActivityIndicator, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
 
+import * as albumActions from '../actions/albums';
 import theme from '../constants/theme';
-
-// temporal data will then be fetched from the mocked server
-const mockData = {
-  id: '001',
-  fullUrl: 'https://wallpapercave.com/wp/sS1W57t.jpg'
-};
 
 class PhotoDetailScreen extends PureComponent {
   static navigationOptions = {
@@ -18,20 +16,72 @@ class PhotoDetailScreen extends PureComponent {
     headerTransparent: true
   }
 
+  constructor(props) {
+    super(props);
+    this.renderContent = this.renderContent.bind(this);
+  }
+
+  componentWillMount() {
+    const { navigation, fetchPhotoDetail } = this.props;
+    const photoId = navigation.getParam('photoId');
+    fetchPhotoDetail(photoId);
+  }
+
+  renderContent() {
+    const { isFetching, success, error, photo } = this.props;
+    let content = null;
+    if (isFetching) {
+      content = (
+        <View style={styles.centeredContentContainer}>
+          <ActivityIndicator size="large" color={theme.white} />
+        </View>
+      );
+    }
+    if (success) {
+      content = (
+        <View style={styles.container}>
+          <Image resizeMode="contain" style={styles.photo} source={{ uri: photo.fullUrl }} />
+        </View>
+      );
+    }
+    if (error) {
+      content = (
+        <View style={styles.centeredContentContainer}>
+          <Text>Error. Reintentar en unos minutos.</Text>
+        </View>
+      );
+    }
+    return content;
+  }
+
   render() {
     return (
-      <View style={styles.container}>
+      <React.Fragment>
         <StatusBar backgroundColor={theme.black} />
-        <Image resizeMode="contain" style={styles.photo} source={{ uri: mockData.fullUrl }} />
-      </View>
+        {this.renderContent()}
+      </React.Fragment>
     );
   }
 }
+
+PhotoDetailScreen.propTypes = {
+  photo: PropTypes.shape().isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  success: PropTypes.bool.isRequired,
+  error: PropTypes.bool.isRequired,
+  fetchPhotoDetail: PropTypes.func.isRequired
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.black
+  },
+  centeredContentContainer: {
+    flex: 1,
+    backgroundColor: theme.black,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   photo: {
     height: '100%',
@@ -39,4 +89,15 @@ const styles = StyleSheet.create({
   }
 });
 
-export default PhotoDetailScreen;
+const mapStateToProps = ({ albumReducer }) => ({
+  isFetching: albumReducer.photoDetail.isFetching,
+  success: albumReducer.photoDetail.success,
+  error: albumReducer.photoDetail.error,
+  photo: albumReducer.photoDetail.data
+});
+
+const mapDispatchToProps = dispatch => (
+  bindActionCreators(albumActions, dispatch)
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(PhotoDetailScreen);
--- src/screens/test/PhotoDetailScreen.test.js
+++ src/screens/test/PhotoDetailScreen.test.js
@@ -1,11 +1,73 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
 import PhotoDetailScreen from '../PhotoDetailScreen';
 
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const mockPhotoDetail = {
+  id: 'mockId',
+  fullUrl: 'mockUrl'
+};
+
+const mockReducers = {
+  albumReducer: {
+    photoDetail: {
+      isFetching: true,
+      success: false,
+      error: false,
+      data: {}
+    }
+  }
+};
+
+const fetchingStore = mockStore(mockReducers);
+const successStore = mockStore({
+  albumReducer: {
+    photoDetail: {
+      ...mockReducers.albumReducer.photoDetail,
+      isFetching: false,
+      success: true,
+      data: mockPhotoDetail
+    }
+  }
+});
+const errorStore = mockStore({
+  albumReducer: {
+    photoDetail: {
+      ...mockReducers.albumReducer.photoDetail,
+      error: true
+    }
+  }
+});
+
+const mockNavigation = {
+  getParam: jest.fn()
+};
+
 describe('PhotoDetailScreen test suite', () => {
-  it('Should match snapshot', () => {
-    const component = create(<PhotoDetailScreen />).toJSON();
+  beforeEach(() => {
+    mockNavigation.getParam.mockClear();
+  });
+  it('Should match snapshot (isFetching case)', () => {
+    const component = create(
+      <PhotoDetailScreen navigation={mockNavigation} store={fetchingStore} />
+    ).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+  it('Should match snapshot (success case)', () => {
+    const component = create(
+      <PhotoDetailScreen navigation={mockNavigation} store={successStore} />
+    ).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+  it('Should match snapshot (error case)', () => {
+    const component = create(
+      <PhotoDetailScreen navigation={mockNavigation} store={errorStore} />
+    ).toJSON();
     expect(component).toMatchSnapshot();
   });
 });
